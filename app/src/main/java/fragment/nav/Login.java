14a5704f871aea4d1ae50425ec6e58a9b3b5f49e
Hoang Do <hoang.dv@neo-lab.vn>
2016-09-22 13:48:00 +0700

    fragment

---
+++ app/src/main/java/fragment/nav/Login.java
@@ -0,0 +1,19 @@
+package fragment.nav;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import luanz.nguyenthanhluan.demozingmp3.R;
+
+/**
+ * Created by PC on 9/22/2016.
+ */
+public class Login extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
--- app/src/main/java/luanz/nguyenthanhluan/demozingmp3/MainActivity.java
+++ app/src/main/java/luanz/nguyenthanhluan/demozingmp3/MainActivity.java
@@ -1,7 +1,10 @@
 package luanz.nguyenthanhluan.demozingmp3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,6 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import fragment.nav.FragmentAlbum;
+import fragment.nav.FragmentMusicHot;
+import fragment.nav.FragmentMyMp3;
+import fragment.nav.FragmentNghesi;
+import fragment.nav.FragmentVideo;
+import fragment.nav.Login;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -67,18 +77,25 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment fragment = null;
         if (id == R.id.nav_dangnhap) {
-            // Handle the camera action
+            startActivity(new Intent(getApplicationContext(), Login.class));
         } else if (id == R.id.nav_music) {
+            fragment = new FragmentMyMp3();
+        }else if(id==R.id.nav_nhachot){
+            fragment = new FragmentMusicHot();
+        }else if (id==R.id.nav_album){
+            fragment = new FragmentAlbum();
+        }else if (id==R.id.nav_video){
+            fragment = new FragmentVideo();
+        }else if (id == R.id.nav_nghesi){
+            fragment = new FragmentNghesi();
+        }
 
-        } else if (id == R.id.nav_nhachot) {
-
-        } else if (id == R.id.nav_album) {
-
-        } else if (id == R.id.nav_video) {
-
-        } else if (id == R.id.nav_nghesi) {
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frame_container, fragment).commit();
 
         }
 
